f72bebeffc457aeb35746cdb72aa425e1254813b
Aymerick <aymerick@fotonauts.com>
2013-11-15 14:25:45 +0100

    rails new activr_demo --skip-test-unit --skip-active-record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActivrDemo::Application.config.secret_token = '6d2ccb1d686791af72f37f8e85c6e15f394e4bd9165aaea5c40b1caba0241827edff5296035d4de04d3054a18f19ba28797f31174a138c66b0369a8709bb14c5'
