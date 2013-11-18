2e0b5de2e33cdb765814cf1ae24b5c36ded81ae2
Aymerick <aymerick@fotonauts.com>
2013-11-18 11:09:52 +0100

    'rails generate controller Home index' and setup advised by devise

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module ActivrDemo
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Per devise advise "If you are deploying on Heroku with Rails 3.2 only"
+    config.assets.initialize_on_precompile = false
   end
 end
