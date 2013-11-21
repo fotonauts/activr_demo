e2ebbf5cab719535a3dac62508f8be33b8e5ae22
Aymerick <aymerick@fotonauts.com>
2013-11-21 15:32:56 +0100

    fix strong parameters stuff for devise / redirect to user profile on sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # cf. https://github.com/plataformatec/devise#strong-parameters
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :first_name
+    devise_parameter_sanitizer.for(:sign_up) << :last_name
+  end
 end
