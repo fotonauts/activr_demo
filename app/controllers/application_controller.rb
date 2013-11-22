c169f82e06a2a1690c22ca365a55eb5113d0005f
Aymerick <aymerick@fotonauts.com>
2013-11-22 11:27:16 +0100

    Fix ApplicationController#after_sign_in_path_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,21 @@ class ApplicationController < ActionController::Base
   # cf. https://github.com/plataformatec/devise#strong-parameters
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  # cf. https://github.com/plataformatec/devise/wiki/How-To:-Redirect-back-to-current-page-after-sign-in,-sign-out,-sign-up,-update
+  after_filter :store_location
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    if (request.fullpath != "/users/sign_in" &&
+        request.fullpath != "/users/sign_up" &&
+        request.fullpath != "/users/password" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath
+    end
+  end
 
   def after_sign_in_path_for(resource)
-    user_path(current_user)
+    session[:previous_url] || root_path
   end
 
 
