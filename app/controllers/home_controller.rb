c7e03562b7675559cf94b15ca33813bab5d8463a
Aymerick <aymerick@fotonauts.com>
2013-11-27 15:55:30 +0100

    Enabling infinitescroll to display global activity feed on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
   def index
+    page_size = 20
+    page = (params[:page] || 1).to_i
+    skip = (page - 1) * page_size
+
     # fetch last activities
-    @activities = Activr.activities(50)
+    ary      = Activr.activities(page_size, :skip => skip)
+    total_nb = Activr.count_activities
+
+    # paginate
+    @activities = Kaminari.paginate_array(ary, :total_count => total_nb).page(page).per(page_size)
   end
 end
