d88295f966d556d569348dc51e69ef850ec7b54b
Aymerick <aymerick@fotonauts.com>
2013-12-12 11:53:55 +0100

    always redirect to user profile after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,21 +6,9 @@ class ApplicationController < ActionController::Base
   # cf. https://github.com/plataformatec/devise#strong-parameters
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  # cf. https://github.com/plataformatec/devise/wiki/How-To:-Redirect-back-to-current-page-after-sign-in,-sign-out,-sign-up,-update
-  after_filter :store_location
-
-  def store_location
-    # store last url - this is needed for post-login redirect to whatever the user last visited.
-    if (request.fullpath != "/users/sign_in" &&
-        request.fullpath != "/users/sign_up" &&
-        request.fullpath != "/users/password" &&
-        !request.xhr?) # don't store ajax calls
-      session[:previous_url] = request.fullpath
-    end
-  end
 
   def after_sign_in_path_for(resource)
-    session[:previous_url] || root_path
+    url_for(current_user)
   end
 
 
